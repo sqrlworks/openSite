d14972596efb8529a047b6d37ac379dd47d0e0a4
sqrlworks <o.ryabaya@gmail.com>
2019-06-28 11:27:05 -0400

    add functionality to take multiple urls as arguments

--- openSite_run.py
+++ openSite_run.py
@@ -3,18 +3,28 @@
 
 import sys, os
 
-if len(sys.argv) < 2:
-    print('Usage: py openSite_run.py [url] \nOpens url in default browser')
+argv_len = len(sys.argv)
+
+if argv_len < 2:
+    print('Usage: py openSite_run.py [urls] \nOpens urls in default browser')
     sys.exit()
 
-print ('Argument List: ' + str(sys.argv))
-url = sys.argv[1]
-
 if sys.platform.startswith('win'):
-	try:
-		os.startfile(url)
-		print("Running on Windows: " + sys.platform)
-	except:
-		print("Error: Invalid URL")
+	print("1. Running on Windows: " + sys.platform)
+	print ('2. Argument List: ' + str(sys.argv))
+	invalid_URL = []
+
+	for i in range (1,argv_len):
+		url = sys.argv[i]
+		try:
+			os.startfile(url)
+		except:
+			invalid_URL.append(url)
+
+	if invalid_URL:
+		print('3. Error - Invalid URL for: ')
+		print(*invalid_URL, sep=', ')
+		print('Make sure to include prefix: www|http:|https:')
+
 else:
 	print("Functionality for Windows only")
