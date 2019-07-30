749cdec344e5ae0443f6a5cd6399562864ffa350
sqrlworks <o.ryabaya@gmail.com>
2019-07-30 14:13:03 -0400

    Rename file

--- openSiteRun.py
+++ openSiteRun.py
@@ -1,12 +1,12 @@
-# openSite_run.py - Opens user input: url in user's default browser from the command line
-#Functionaliy platform dependent - for Windows only
+# openSiteRun.py - Opens user input/url(s) in user's default browser from the command line
+# Functionaliy platform dependent - for Windows only
 
 import sys, os
 
 argv_len = len(sys.argv)
 
 if argv_len < 2:
-    print('Usage: py openSite_run.py [urls] \nOpens urls in default browser')
+    print('Usage: py openSiteRun.py [urls] \nOpens urls in default browser')
     sys.exit()
 
 if sys.platform.startswith('win'):
@@ -22,9 +22,7 @@ if sys.platform.startswith('win'):
 			invalid_URL.append(url)
 
 	if invalid_URL:
-		print('3. Error - Invalid URL for: ')
-		print(*invalid_URL, sep=', ')
-		print('Make sure to include prefix: www|http:|https:')
+		print('Error: Invlaid URL for: ' + ', '.join(invalid_URL) +'\nMake sure to include prefix: www|http:|https:')
 
 else:
 	print("Functionality for Windows only")
