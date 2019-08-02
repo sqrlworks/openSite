1a810bd67f1d133799f968249c0ae9bea71eb6b1
sqrlworks <o.ryabaya@gmail.com>
2019-08-01 21:23:41 -0400

    add sleep() function to code execution

--- openSiteRun.py
+++ openSiteRun.py
@@ -1,7 +1,7 @@
 # openSiteRun.py - Opens user input/url(s) in user's default browser from the command line
 # Functionaliy platform dependent - for Windows only
 
-import sys, os, colorama
+import sys, os, time, colorama
 from colorama import Fore, Back, Style
 
 colorama.init()
@@ -9,23 +9,26 @@ colorama.init()
 argv_len = len(sys.argv)
 
 if argv_len < 2:
-    print('Usage: py openSiteRun.py [urls] \nOpens urls in default browser')
+    print('\nUsage: py openSiteRun.py [urls] \nOpens URLs in default browser\n')
     sys.exit()
 
 if sys.platform.startswith('win'):
-	print("\n1. Running on Windows: " + sys.platform)
-	print ('2. Argument List: ' + str(sys.argv) + '\n')
+
 	invalid_URL = []
 
 	for i in range (1,argv_len):
 		url = sys.argv[i]
 		try:
 			os.startfile(url)
+			time.sleep(3)
+			print("\n1. Running on Windows: " + sys.platform)
+			print ('2. Argument List: ' + str(sys.argv) + '\n')
 		except:
 			invalid_URL.append(url)
 
 	if invalid_URL:
-		print(Fore.WHITE + Back.RED + 'Error:' + Style.RESET_ALL+ ' Invalid URL for: ' + ', '.join(invalid_URL) +'\nMake sure to include ' + Fore.WHITE + Back.GREEN + 'prefix: www|http:|https:\n')
+		print(Fore.WHITE + Back.RED + '\nError: Invalid URL for: ' + ', '.join(invalid_URL) 
+			+ Style.RESET_ALL + '\nMake sure to include ' + Fore.WHITE + Back.BLACK + 'prefix: www|http:|https:\n')
 
 else:
 	print("Functionality for Windows only")
